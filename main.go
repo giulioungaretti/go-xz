6c64276c97bda90b7cc95a7c81c78814f060a83e
Giulio Ungaretti <giulioungaretti@me.com>
2015-03-26 15:44:21 +0100

    Added all required functions for now.

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"bytes"
+	"errors"
+	"flag"
 	"io"
 	"log"
 	"os"
@@ -8,58 +11,92 @@ import (
 )
 
 func main() {
+	fp := flag.String("file", "", "name of file")
+	inflate := flag.Bool("inflate", false, "inflate archive")
+	deflate := flag.Bool("deflate", false, "deflate archive")
+	stdout := flag.Bool("stdout", false, "write to file or to stdout when inflating")
+	flag.Parse()
 	// path
-	fp := "./testdata.xz"
-	r := xzReader(fp)
-	// copy to stdout
-	n, err := io.Copy(os.Stdout, r)
+	if *deflate {
+		err := xzWriter(*fp)
+		if err != nil {
+			log.Printf("Err: %v", err)
+		}
+	}
+	if *inflate {
+		if *stdout {
+			r, err := xzReader(*fp, *stdout)
+			n, err := io.Copy(os.Stdout, r)
+			if err != nil {
+				log.Printf("copied %d bytes with err: %v", n, err)
+			} else {
+				log.Printf("copied %d bytes", n)
+			}
+		} else {
+			_, err := xzReader(*fp, *stdout)
+			if err != nil {
+				log.Printf("Err: %v", err)
+			}
+		}
+
+	}
+}
+
+func xzReader(file string, stdout bool) (io.ReadCloser, error) {
+	f, err := os.Open(file)
 	if err != nil {
-		log.Printf("copied %d bytes with err: %v", n, err)
+		log.Fatal(err)
+	}
+	if stdout {
+		rpipe, wpipe := io.Pipe()
+		cmd := exec.Command("xz", "--decompress", "--stdout")
+		cmd.Stdin = f
+		cmd.Stdout = wpipe
+		// print the error
+		cmd.Stderr = os.Stderr
+		go func() {
+			err := cmd.Run()
+			wpipe.CloseWithError(err)
+			defer f.Close()
+		}()
+		return rpipe, err
 	} else {
-		log.Printf("copied %d bytes", n)
+		// Create an *exec.Cmd
+		cmd := exec.Command("xz", "--decompress", file)
+		// Stdout buffer
+		cmdOutput := &bytes.Buffer{}
+		// Attach buffer to command
+		cmd.Stdout = cmdOutput
+		// Stderr buffer
+		cmderror := &bytes.Buffer{}
+		// Attach buffer to command
+		cmd.Stderr = cmderror
+		// Execute command
+		err := cmd.Run() // will wait for command to return
+		if err != nil {
+			errstr := string(cmderror.Bytes())
+			err = errors.New(errstr)
+		}
+		return nil, err
 	}
 }
 
-func xzReader(file string) io.ReadCloser {
-	f, err := os.Open(file)
+func xzWriter(file string) error {
+	// Create an *exec.Cmd
+	cmd := exec.Command("xz", file)
+	//  buffer
+	cmdOutput := &bytes.Buffer{}
+	// Attach buffer to command
+	cmd.Stdout = cmdOutput
+	// Stderr buffer
+	cmderror := &bytes.Buffer{}
+	// Attach buffer to command
+	cmd.Stderr = cmderror
+	// Execute command
+	err := cmd.Run() // will wait for command to return
 	if err != nil {
-		log.Fatal(err)
+		errstr := string(cmderror.Bytes())
+		err = errors.New(errstr)
 	}
-	defer f.Close()
-
-	rpipe, wpipe := io.Pipe()
-	cmd := exec.Command("xz", "--decompress", "--stdout")
-	cmd.Stdin = f
-	cmd.Stdout = wpipe
-
-	go func() {
-		err := cmd.Run()
-		wpipe.CloseWithError(err)
-	}()
-
-	return rpipe
-}
-
-func xzCompresser(file string) io.ReadCloser {
-	f, err := os.Open(file)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer f.Close()
-
-	rpipe, wpipe := io.Pipe()
-
-	cmd := exec.Command("xz", "--stdout")
-
-	cmd.Stdin = f
-	cmd.Stdout = wpipe
-
-	go func() {
-		err := cmd.Run()
-		wpipe.CloseWithError(err)
-	}()
-	//TODO
-	// return the new file
-	// and delete the old one if no error
-	return rpipe
+	return err
 }
